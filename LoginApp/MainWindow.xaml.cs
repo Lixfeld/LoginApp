cc604c300419575350f69049f7044e9880125f0d
Lixfeld <lixfeld-git@gmx.de>
2020-03-08 13:26:12 +0100

    Add sample project

---
+++ LoginApp/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using ReactiveUI;
+using ReactiveUI.Validation.Extensions;
+
+namespace LoginApp
+{
+    public partial class MainWindow : ReactiveWindow<MainWindowModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MainWindowModel();
+
+            userNameTextBox.MaxLength = 16;
+            passwordTextBox.MaxLength = 16;
+            confirmPwTextBox.MaxLength = 16;
+
+            this.WhenActivated(disposables =>
+            {
+                CreateBindings(disposables);
+                SetupValidationBindings(disposables);
+
+                this.ViewModel.ConfirmSignUp.RegisterHandler(async context =>
+                {
+                    dialogHostTextBlock.Text = $"User '{context.Input}' created successfully.";
+                    await DialogHost.Show(signUpHost.DialogContent);
+                    context.SetOutput(Unit.Default);
+                }).DisposeWith(disposables);
+            });
+        }
+
+        private void CreateBindings(CompositeDisposable disposables)
+        {
+            this.Bind(ViewModel, x => x.UserName, x => x.userNameTextBox.Text).DisposeWith(disposables);
+            this.Bind(ViewModel, x => x.Password, x => x.passwordTextBox.Text).DisposeWith(disposables);
+            this.Bind(ViewModel, x => x.ConfirmPassword, x => x.confirmPwTextBox.Text).DisposeWith(disposables);
+            this.BindCommand(ViewModel, x => x.SignUp, x => x.signUpButton).DisposeWith(disposables);
+        }
+
+        private void SetupValidationBindings(CompositeDisposable disposables)
+        {
+            //Use only one BindValidation against a property (UserName) - [BUG] Index out of range with multiple validators #34
+            //https://github.com/reactiveui/ReactiveUI.Validation/issues/34
+
+            this.BindValidation(ViewModel, vm => vm.UserName, v => v.userNameErrorTextBlock.Text)
+                .DisposeWith(disposables);
+            this.BindValidation(ViewModel, vm => vm.PasswordRule, v => v.passwordErrorTextBlock.Text)
+                .DisposeWith(disposables);
+            this.BindValidation(ViewModel, vm => vm.ConfirmPasswordRule, v => v.confirmPwErrorTextBlock.Text)
+                .DisposeWith(disposables);
+
+            this.WhenAnyValue(x => x.ViewModel.ValidationContext.IsValid)
+                .Select(v => v)
+                .BindTo(this, x => x.summaryHeaderTextBlock.Visibility, BooleanToVisibilityHint.Inverse)
+                .DisposeWith(disposables);
+
+            this.WhenAnyValue(x => x.ViewModel.ValidationContext.Text)
+                .Select(vt => vt.ToSingleLine(Environment.NewLine))
+                .BindTo(this, v => v.summaryContentTextBlock.Text)
+                .DisposeWith(disposables);
+        }
+    }
+}
---
+++ LoginApp/MainWindowModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Helpers;
+
+namespace LoginApp
+{
+    public class MainWindowModel : ReactiveValidationObject<MainWindowModel>
+    {
+        public ValidationHelper PasswordRule { get; }
+
+        public ValidationHelper ConfirmPasswordRule { get; }
+
+        [Reactive]
+        public string UserName { get; set; }
+
+        [Reactive]
+        public string Password { get; set; }
+
+        [Reactive]
+        public string ConfirmPassword { get; set; }
+
+        public ReactiveCommand<Unit, Unit> SignUp { get; }
+
+        public Interaction<string, Unit> ConfirmSignUp { get; } = new Interaction<string, Unit>();
+
+        public MainWindowModel()
+        {
+            //Username: MinLength 3 and only alphanumeric characters
+            this.ValidationRule(vm => vm.UserName,
+                name => name?.Length >= 3 && Regex.IsMatch(name, @"^[A-Za-z0-9]*$"),
+                name => name?.Length < 3 ? "Username must be at least 3 chars." : $"'{name}' contains invalid characters.");
+
+            PasswordRule = this.ValidationRule(vm => vm.Password, pw => pw?.Length >= 6, "Password must be at least 6 chars.");
+
+            IObservable<bool> passwordsMatch = this.WhenAnyValue(x => x.Password, x => x.ConfirmPassword, (pw1, pw2) => pw1 == pw2);
+            ConfirmPasswordRule = this.ValidationRule(
+                _ => passwordsMatch,
+                (vm, match) => match ? string.Empty : "Passwords must match.");
+
+            SignUp = ReactiveCommand.CreateFromTask(async _ => await ConfirmSignUp.Handle(UserName), canExecute: this.IsValid());
+        }
+    }
+}
