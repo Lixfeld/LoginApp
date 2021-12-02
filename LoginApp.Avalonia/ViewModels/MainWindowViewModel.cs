f1e416de53f60b9c1225be31e87465483f696f02
Lixfeld <lixfeld-git@gmx.de>
2021-12-02 21:07:52 +0100

    Add ReactiveUI.Validation rules

--- LoginApp.Avalonia/ViewModels/MainWindowViewModel.cs
+++ LoginApp.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Abstractions;
+using ReactiveUI.Validation.Contexts;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Helpers;
 
 namespace LoginApp.Avalonia.ViewModels
 {
@@ -26,8 +31,16 @@ namespace LoginApp.Avalonia.ViewModels
 
         public MainWindowViewModel()
         {
-            IObservable<bool> passwordsMatch = this.WhenAnyValue(x => x.Password, x => x.ConfirmPassword, (pw1, pw2) => pw1 == pw2 && !string.IsNullOrEmpty(pw1));
-            SignUp = ReactiveCommand.CreateFromTask(async () => await ConfirmSignUp.Handle(UserName), canExecute: passwordsMatch);
+            //Username: MinLength 3 and only alphanumeric characters
+            this.ValidationRule(vm => vm.UserName,
+                name => name?.Length >= 3 && Regex.IsMatch(name, @"^[A-Za-z0-9]*$"),
+                name => name?.Length < 3 ? "Username must be at least 3 chars." : "Username contains invalid characters.");
+
+            IObservable<bool> passwordsMatch = this.WhenAnyValue(x => x.Password, x => x.ConfirmPassword, (pw1, pw2) => pw1 == pw2);
+            this.ValidationRule(vm => vm.Password, pw => pw?.Length >= 6, "Password must be at least 6 chars.");
+            this.ValidationRule(vm => vm.ConfirmPassword, passwordsMatch, "Passwords must match.");
+
+            SignUp = ReactiveCommand.CreateFromTask(async () => await ConfirmSignUp.Handle(UserName), canExecute: this.IsValid());
         }
     }
 }
--- LoginApp.Avalonia/ViewModels/ViewModelBase.cs
+++ LoginApp.Avalonia/ViewModels/ViewModelBase.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ReactiveUI;
+using ReactiveUI.Validation.Helpers;
 
 namespace LoginApp.Avalonia.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    public class ViewModelBase : ReactiveValidationObject
     {
     }
 }
